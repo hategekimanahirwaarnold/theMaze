df4b17f1a04b9ab7c93ce4d24bcc6fd8063600d6
Hirwa Arnold <hategekimanahirwaarnold@gmail.com>
2024-05-16 04:24:38 +0200

    Add all header files

---
+++ src/headers/maze.h
@@ -0,0 +1,43 @@
+#ifndef MAZE_H
+#define MAZE_H
+
+#define SCREEN_WIDTH 1280
+#define SCREEN_HEIGHT 800
+#define TEX_COUNT 6
+#define TEX_WIDTH 64
+#define TEX_HEIGHT 64
+#define MAP_WIDTH 24
+#define MAP_HEIGHT 24
+#define MAP_CHAR_COUNT 1275
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <math.h>
+#include "structs.h"
+#include "global.h"
+
+int *readMap(char *filename, int *map);
+void openTextures(char *mapName);
+bool init(void);
+void updateRenderer(bool textured);
+void close_file(void);
+void handle_inputs(int *maze);
+bool quit(void);
+void cast_ray(int *maze, bool textured);
+void show_walls(int *maze, SDL_Point map, point_t rayPos, point_t rayDir,
+			double distToWall, int x, int side, bool textured);
+
+void show_flat_background(void);
+void show_textured_background(SDL_Point map, point_t rayDir, double distToWall,
+					double wallX, int drawEnd, int x, int side);
+
+#endif /* MAZE_H */
---
+++ src/headers/structs.h
@@ -0,0 +1,16 @@
+#ifndef STRUCTS_H
+#define STRUCTS_H
+
+/**
+ * struct point_s - holds x/y point coordinates of a double type
+ * @x: X coordinate
+ * @y: Y coordinate
+ * Description: struct point
+ */
+typedef struct point_s
+{
+	double x;
+	double y;
+} point_t;
+
+#endif /* STRUCTS_H */
